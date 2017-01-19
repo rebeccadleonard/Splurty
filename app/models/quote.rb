b47105601c50e41fdd20a5e41fbae3dc587380e3
Rebecca Leonard <rebeccadleonard@gmail.com>
2017-01-19 20:46:57 +0000

    added length limits to submissions

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+  validates :saying, presence: true, length: { maximun: 150, minimum: 10}
+  validates :author, presence: true, length: {maximum: 50, minimum: 3}
 end
