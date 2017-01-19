639fb02ee32d550d3bd0cce781bfb9f5ed128d22
Rebecca Leonard <rebeccadleonard@gmail.com>
2017-01-19 19:27:40 +0000

    submit button adds quote to database

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,4 +6,15 @@ class QuotesController < ApplicationController
   def new
     @quote = Quote.new
   end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private #look this up#
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end    
 end
